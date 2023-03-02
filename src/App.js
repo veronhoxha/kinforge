f7d5b5d92033fdbb6113a4d763683ff3357ff7ed
Veron <veronbhoxha@gmail.com>
2023-03-02 10:50:18 +0100

    Added navigation bar and redirected the links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './app/components/navbar';
+import Login from './app/pages/login/login';
+import Signup from './app/pages/signup/signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar/>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={Signup}/>
+        </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 
---
+++ src/app/components/navbar.js
@@ -0,0 +1,45 @@
+import { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import '../styles/navbar.css';
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
+
+  return (
+    <header>
+      <div>
+        <Link to="/">
+          <img src={process.env.PUBLIC_URL + 'favicon.ico'} alt="Logo"></img>
+        </Link>
+      </div>
+
+      <nav className="nav-wrapper ml-auto" ref={navRef}>
+        <Link to="/login">
+          <Button variant="contained" style={{ backgroundColor: 'white', color: '#7D4032' }}>
+            Log In
+          </Button>
+        </Link>
+        <Link to="/signup">
+          <Button variant="contained" style={{ backgroundColor: '#7D4032', color: 'white' }}>
+            Sign Up
+          </Button>
+        </Link>
+
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes/>
+        </button>
+      </nav>
+        <button className="nav-btn" onClick={showNavBar}>
+            <FaBars/>
+        </button>
+    </header>
+  );
+}
+
+export default Navbar;
