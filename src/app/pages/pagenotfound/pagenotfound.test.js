ec3236ef5e67ee521c4c13385a02f704dba809e0
Veron <veronbhoxha@gmail.com>
2023-04-20 23:03:03 +0200

    Added test cases for page not found page

---
+++ src/app/pages/pagenotfound/pagenotfound.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import PageNotFound from './pagenotfound';
+
+test('renders PageNotFound component without crashing', () => {
+    render(
+      <Router>
+        <PageNotFound />
+      </Router>
+    );
+});
+
+test('displays "Oops! Page was not found." message', () => {
+    render(
+        <Router>
+        <PageNotFound />
+        </Router>
+    );
+    expect(screen.getByText(/Oops! Page was not found./i)).toBeInTheDocument();
+});
+
+test('displays "404" error code', () => {
+    render(
+      <Router>
+        <PageNotFound />
+      </Router>
+    );
+    expect(screen.getByText(/404/i)).toBeInTheDocument();
+});
+
+test('"Go back home" button is present and clickable', () => {
+    render(
+      <Router>
+        <PageNotFound />
+      </Router>
+    );
+    const goBackHomeButton = screen.getByRole('button', { name: /go back home/i });
+    expect(goBackHomeButton).toBeInTheDocument();
+});
+
+test('"Go back home" button navigates to the homepage', () => {
+    render(
+      <Router>
+        <PageNotFound />
+      </Router>
+    );
+    const goBackHomeButtonLink = screen.getByRole('link', { name: /go back home/i });
+    expect(goBackHomeButtonLink.closest('a')).toHaveAttribute('href', '/');
+});
+  
+  
+
+
+  
+  
