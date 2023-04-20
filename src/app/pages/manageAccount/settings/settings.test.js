64cdfc6088c04c263a2c8fb45176d54bfe410b43
Veron <veronbhoxha@gmail.com>
2023-04-20 17:29:02 +0200

    Added test cases for settings page

---
+++ src/app/pages/manageAccount/settings/settings.test.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Settings from './settings';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { BrownSwitch } from './settings';
+
+jest.mock('firebase/auth', () => {
+    return {
+      getAuth: jest.fn(() => {
+        return {
+          currentUser: {
+            uid: '123',
+            email: 'test@example.com',
+          },
+          onAuthStateChanged: jest.fn((callback) => {
+            callback({ uid: '123' });
+            return () => {}; 
+          }),
+        };
+      }),
+    };
+  });
+
+jest.mock('firebase/firestore', () => {
+  return {
+    getFirestore: jest.fn(() => {}),
+    doc: jest.fn(() => {}),
+    setDoc: jest.fn(() => {}),
+    getDoc: jest.fn(() => {
+      return {
+        exists: () => true,
+        data: () => ({ activeSwitch: 1 }),
+      };
+    }),
+  };
+});
+
+describe('Settings', () => {
+  test('renders change password button', () => {
+    render(<Settings />);
+    const changePasswordButton = screen.getByText('Change Password');
+    expect(changePasswordButton).toBeInTheDocument();
+  });
+
+  test('renders hierarchy design button', () => {
+    render(<Settings />);
+    const hierarchyDesignButton = screen.getByText('Hierarchy design');
+    expect(hierarchyDesignButton).toBeInTheDocument();
+  });
+
+  test('shows and hides password form when change password button is clicked', async () => {
+    render(<Settings />);
+    const changePasswordButton = screen.getByText('Change Password');
+
+    userEvent.click(changePasswordButton);
+    const currentPasswordLabel = await screen.findByText('Current Password:');
+    expect(currentPasswordLabel).toBeInTheDocument();
+
+    userEvent.click(changePasswordButton);
+    await waitFor(() => {
+      expect(currentPasswordLabel).not.toBeInTheDocument();
+    });
+  });
+
+  test('shows and hides hierarchy design form when hierarchy design button is clicked', async () => {
+    render(<Settings />);
+    const hierarchyDesignButton = screen.getByText('Hierarchy design');
+
+    userEvent.click(hierarchyDesignButton);
+
+    const rectangleNodesLabel = await screen.findByText('Rectangle Nodes');
+    const ellipseNodesLabel = await screen.findByText('Ellipse Nodes');
+    const ovalNodesLabel = await screen.findByText('Oval Nodes');
+    expect(rectangleNodesLabel).toBeInTheDocument();
+    expect(ellipseNodesLabel).toBeInTheDocument();
+    expect(ovalNodesLabel).toBeInTheDocument();
+
+    userEvent.click(hierarchyDesignButton);
+    await waitFor(() => {
+      expect(rectangleNodesLabel).not.toBeInTheDocument();
+      expect(ellipseNodesLabel).not.toBeInTheDocument();
+      expect(ovalNodesLabel).not.toBeInTheDocument();
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
