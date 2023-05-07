8eae557c39cde7bba1341e1ab23719850cef1e43
Veron <veronbhoxha@gmail.com>
2023-05-07 11:38:11 +0200

    Fixed the bug with blocking everything in the background when modal is open

--- src/app/components/FamilyModal.js
+++ src/app/components/FamilyModal.js
@@ -4,37 +4,42 @@ import '../styles/familyModal.css';
 import { Link } from 'react-router-dom';
 
 const FamilyModal = ({ open, onClose }) => {
+
   if (!open) return null;
+  
   return (
-    <div onClick={onClose} className='overlay' data-testid='overlay'>
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        data-testid='modalContainer'
-        className='modalContainer'
-      >
-        <img src={family_pic} alt='Family' className='modalImg'/>
-        <div className='modalRight'>
-          <div className='content'>
-            <p className='modalText'>Do you want to continue with </p>
-            <h1 className='modalTitle'>Dad's family side or Mom's family side ?</h1>
-        </div>
-        <div className='btnContainer'>
-          <Link to='/familyTree' className='linkbtnPrimary'>
-            <button className='btnPrimary' onClick={onClose}>
-              <span className='bold'>Dad's side</span>
-            </button>
-          </Link>
-          <Link to='/familyTreeMom' className='linkbtnOutline'>
-            <button className='btnOutline' onClick={onClose}>
-              <span className='bold'>Mom's side</span>
-            </button>
-          </Link>
+    <>
+      <div className='block-background'></div>
+        <div onClick={onClose} className='overlay' data-testid='overlay'>
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            data-testid='modalContainer'
+            className='modalContainer'
+          >
+            <img src={family_pic} alt='Family' className='modalImg'/>
+            <div className='modalRight'>
+              <div className='content'>
+                <p className='modalText'>Do you want to continue with </p>
+                <h1 className='modalTitle'>Dad's family side or Mom's family side ?</h1>
+            </div>
+            <div className='btnContainer'>
+              <Link to='/familyTree' className='linkbtnPrimary'>
+                <button className='btnPrimary' onClick={onClose}>
+                  <span className='bold'>Dad's side</span>
+                </button>
+              </Link>
+              <Link to='/familyTreeMom' className='linkbtnOutline'>
+                <button className='btnOutline' onClick={onClose}>
+                  <span className='bold'>Mom's side</span>
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </>
   );
 };
 
