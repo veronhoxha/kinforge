a92be69aa32b4b4cf85b81466272abb64d2cc288
Veron <veronbhoxha@gmail.com>
2023-05-08 11:53:10 +0200

    Small modifcation

--- src/app/pages/hierarchy/Places.js
+++ src/app/pages/hierarchy/Places.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useMemo, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
