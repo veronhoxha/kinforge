1ad2ecb5b8cb0b44f64ebca4acc79b78adf16256
Veron <veronbhoxha@gmail.com>
2023-05-10 11:20:18 +0200

    Added more test cases for SearchBar component

--- src/app/components/SearchBar.test.js
+++ src/app/components/SearchBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import SearchBar from './SearchBar';
@@ -12,6 +12,53 @@ describe('SearchBar component', () => {
                 <SearchBar />
             </Router>
         );
+    });
+
+    test('shows search bar input correctly', () => {
+        render(
+            <Router>
+                <SearchBar />
+            </Router>
+        );
+        const searchBar = screen.getByTestId('search-bar');
+        expect(searchBar).toBeInTheDocument();
+    });
+
+    test('shows search and clear icons correctly', () => {
+        render(
+            <Router>
+                <SearchBar />
+            </Router>
+        );
+        const searchIcon = screen.getByTestId('search-icon');
+        expect(searchIcon).toBeInTheDocument();
+
+        const clearIcon = screen.queryByTestId('clear-icon');
+        expect(clearIcon).not.toBeInTheDocument();
+    });
+
+    test('does not show search results panel with no search term', () => {
+        render(
+            <Router>
+                <SearchBar />
+            </Router>
+        );
+        const searchResultsPanel = screen.queryByTestId('search-results-panel');
+        expect(searchResultsPanel).not.toBeInTheDocument();
+    });
+
+    test('does not show search results panel with no filtered users', () => {
+        render(
+            <Router>
+                <SearchBar />
+            </Router>
+        );
+
+        const originalUseState = React.useState;
+        React.useState = jest.fn(() => originalUseState([]));
+        const searchResultsPanel = screen.queryByTestId('search-results-panel');
+        expect(searchResultsPanel).not.toBeInTheDocument();
+        React.useState = originalUseState;
     });
 
 });
